8ba179d0b21a3f5438f7e041ae6a74004fb9c970
Unknown <Klaus.Pruenster@gmail.com>
2020-01-22 20:55:57 +0100

    added sudoku class

---
+++ Sudoku.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+type Sudoku struct {
+	s [9][9]int
+}
+
+func New(byteArray []byte) Sudoku {
+	e := Sudoku{}
+	var fullStr = string(byteArray)
+	fullStr = strings.Replace(fullStr, " ", "", -1)
+	fullStr = strings.Replace(fullStr, ".", "0", -1)
+	split := strings.Split(fullStr, "\r\n")
+
+	for i, v := range split {
+		for y, r := range v {
+			e.s[i][y], _ = strconv.Atoi(string(r))
+		}
+	}
+	return e
+}
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"reflect"
-	"strings"
 )
 
 func check(e error) {
@@ -14,35 +13,15 @@ func check(e error) {
 }
 
 func main() {
-	dat, err := ioutil.ReadFile("./sudokus/sudoku.txt")
+	dat, err := ioutil.ReadFile("./sudokus/s.txt")
 	check(err)
+
+	s := Sudoku.New(dat)
+
+	fmt.Println(reflect.TypeOf(dat))
 
 	fmt.Print(string(dat) + "\n")
 
 	fmt.Println(reflect.TypeOf(dat))
 
-	split := strings.Split(string(dat), "\n")
-
-	println(string(split[0]))
-	return
-
-	var sudoku [9][9]int
-
-	var sudokuIdx int = 0
-	for i := 0; i < len(dat); i++ {
-		if string(dat[i]) == "\n" || string(dat[i]) == " " {
-			continue
-		}
-
-		sudokuIdx++
-		print(string(dat[i]))
-
-		if string(dat[i]) == "." {
-			sudoku[sudokuIdx/9][sudokuIdx%9] = 0
-		} else{
-			sudoku[sudokuIdx/9][sudokuIdx%9] = int(dat[i])
-		}
-	}
-
-	fmt.Println(sudoku[0][1])
 }
